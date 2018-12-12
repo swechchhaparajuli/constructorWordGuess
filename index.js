4d6f3e3e2c64730c294157631f6ac7ed5af2067c
swechchhaparajuli <swechchha@gmail.com>
2018-12-11 18:22:52 -0800

    it works

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 var clc = require("cli-color");
 var Word = require("./Word");
 var inquirer = require("inquirer");
-var arrayOfStrings = ["Lord of the Rings", "Lion King", "Little Mermaid"];
+var arrayOfStrings = ["Lord of the Rings", "Little Mermaid", "Lion King", "Love Actually", "La Bamba", "Labrynth", "Les Miserables"];
 var arrayOfWords = [];
 
 var count = 0;
-addsWord();
+
 
  function addsWord(){
+    arrayOfWords = [];
     for(var i = 0; i < arrayOfStrings.length; i++){
         arrayOfWords.push(new Word(arrayOfStrings[i]));
        // console.log(arrayOfWords[i]);
@@ -29,6 +30,7 @@ function startGame(){
         },
     ]).then(function(answers) {
         if (answers.choice == "y"){
+            addsWord();
             //var temp = arrayOfWords[Math.floor(Math.random()*arrayOfWords.length)];
             chosenWord = arrayOfWords[Math.floor(Math.random()*arrayOfWords.length)];
             printedWord = chosenWord.wordConcat();
@@ -55,24 +57,32 @@ playingGame = function(){
             chosenWord.letterGuess(answers.letter);
            // printedWord = "";
             if(chosenWord.bool){
-                console.log(clc.green("CORRECT!!!")); 
+                console.log(clc.green("CORRECT!!!"));
+                console.log("");
             }else{
                 console.log(clc.red("INCORRECT"));
+                console.log("");
             }
 
             printedWord = chosenWord.wordConcat();
 
             console.log(printedWord);
-            console.log(chosenWord.spaceCount)
+            console.log("");
+            //console.log(chosenWord.spaceCount)
+            
             if ((chosenWord.count + chosenWord.spaceCount) == chosenWord.word.length){
                 console.log("You got that right!! Next Word!");
+                console.log("");
                 i = chosenWord.word.length+4;
                 startGame();
+            }else{
+                i++;
+                playingGame();
             }
-            i++;
-            playingGame();
         });
     }else if(i == chosenWord.word.length + 4){
+        console.log("The word was: "+ clc.magenta.bold(chosenWord.word));
+        console.log("Sorry you lost. Better luck on the next word!");
         startGame();
     }
 }
